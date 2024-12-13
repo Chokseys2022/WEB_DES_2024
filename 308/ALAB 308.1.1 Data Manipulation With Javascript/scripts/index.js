c9321e80a5694a2a59277226ddd891d5aff92f5b
SC <chokseysd@gmail.com>
2024-12-13 09:55:40 -0800

    done

--- 308/ALAB 308.1.1 Data Manipulation With Javascript/scripts/index.js
+++ 308/ALAB 308.1.1 Data Manipulation With Javascript/scripts/index.js
@@ -6,42 +6,72 @@ const n2 = 15;
 const n3 = 20;
 const n4 = 5;
 
-// Check one: add up to 50
-// This is a fairly simple operation using
-// arithmetic operators and a comparison.
-const isSum50 = (n1 + n2 + n3 + n4) == 50;
+//Check if all numbers are divisible by 5. Cache the result in a variable.
 
-// Check two: at least two odd numbers
-// Here, we use modulus to check if something is odd.
-// Since % 2 is 0 if even and 1 if odd, we can use
-// arithmetic to count the total number of odd numbers.
-const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+const divN1By5 = n1 % 5 === 0;
+console.log(divN1By5);
 
-// Check three: no number larger than 25
-// This time, we use the OR operator to check
-// if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const divN2By5 = n2 % 5 === 0;
+console.log(divN2By5);
 
-// Check four: all unique numbers
-// This is long, and there are more efficient
-// ways of handling it with other data structures
-// that we will review later.
-const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+const divN3By5 = n3 % 5 === 0;
+console.log(divN3By5);
 
-// Here, we put the results into a single variable 
-// for convenience. Note how we negate isOver25 using
-// the ! operator. We could also have tested for 
-// "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const divN4By5 = n4 % 5 === 0;
+console.log(divN4By5);
 
-// Finally, log the results.
-console.log(isValid);
+console.log("The four numbers are divisble by 5");
 
-// Here's another example of how this COULD be done,
-// but it SHOULD NOT be done this way. As programmers,
-// we break things into small, manageable pieces so that
-// they can be better understood, scaled, and maintained.
-const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
-  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+//Check if the first number is larger than the last. Cache the result in a variable.
+
+const n5 = n1 > n4;
+console.log("Is the first number is larger than the last:", n5);
+
+//Subtract the first number from the second number.
+
+const result = ((n2 - n1) * n3) % n4;
+console.log(`The result of the arithmetic chain is: ${result}`);
+
+//Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+
+const num = 100;
+const isOver25 = num <= 25;
+console.log(isOver25);
+
+//PART 2: PRACTICAL MATH
+
+/*The distance of the trip, in total, is 1,500 miles.
+
+Your car’s fuel efficiency is as follows:
+At 55 miles per hour, you get 30 miles per gallon.
+At 60 miles per hour, you get 28 miles per gallon.
+At 75 miles per hour, you get 23 miles per gallon.
+You have a fuel budget of $175.
+The average cost of fuel is $3 per gallon.*/
+
+//How many gallons of fuel will you need for the entire trip?
+
+const dist = 1500;
+const fuelEffeciency55Miles = 30;
+const fuelNeeded = dist / fuelEffeciency55Miles;
+console.log(`You will need ${fuelNeeded} gallons of fuel for the trip.`);
+
+//Will your budget be enough to cover the fuel expense?
+const fuelBudget = 175;
+const fuelCostPerGal = 3;
+
+//How long will the trip take, in hours?*/
+//const dist = 1500;
+const speed = 55;
+const speed1 = 60;
+const speed2 = 75;
+
+const time = dist / speed;
+const time1 = dist / speed1;
+const time2 = dist / speed2;
+
+console.log(`The trip will take approximately ${time} hours.`);
+console.log(`The trip will take approximately ${time1} hours`);
+console.log(`The trip will take approximately ${time2} hours`);
+
+//PART 3:FUTURE EXPLORATION
