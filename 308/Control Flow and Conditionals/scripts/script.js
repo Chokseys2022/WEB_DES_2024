278616f2046cae0ab51f9cb90d48d6aa6165d5a3
SC <chokseysd@gmail.com>
2024-12-19 17:31:08 -0800

    first commit

---
+++ 308/Control Flow and Conditionals/scripts/script.js
@@ -0,0 +1,5 @@
+const PI = 3.1415;
+const area = PI * radius * radius;
+let radius = 5;
+ 
+console.log(radius)
---
+++ 308/Lesson 308.1 - Intro to Javascript - Part 2 - Operators-Darnell File/scripts/index1.js
@@ -0,0 +1,92 @@
+// Number
+// 10
+// 3.145159
+
+// String
+// "Hello World 100 false true"
+// 'How are you today?'
+// `I am fine thank you!`
+
+// Boolean
+// true
+// false
+
+// Undefined
+// undefined
+
+// Null
+// null
+
+// Object
+// user = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30
+// }
+
+// var
+// var a = 10;
+// var b = 20;
+// var console = 30;
+
+// window.console.log(window.a + window.b);
+
+// let
+// let x = 10; // a declaration.
+// let x; // Can NOT redeclare.
+// x = 30; // CAN Redefine!
+
+// let y; // also a declaration.
+
+// Performs some work with x...
+
+// {
+//     let x = 30;
+//     // Perform some additional with this x...
+// }
+
+// {
+//     let y = 50;
+//     console.log(y * y); // = 400
+// }
+
+// {
+//     let y = 70;
+//     console.log(y * y); // = 400
+// }
+
+// const
+// const PI = 3.14159;
+// const PI = 3.142; // Can NOT be redeclared!
+// PI = 3.14159; // Can NOT be redefined!
+
+// const user = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30
+// }
+// user.firstName = "Jane";
+// console.log(user.firstName);
+
+// Literals
+// console.log(30);
+// console.log("Hello, John Doe");
+// console.log(false);
+// console.log({ firstName: "john", lastName: "doe" });
+
+// 'typeof' keyword
+// console.log(typeof 30);
+// console.log(typeof 3.14159);
+// console.log(typeof "Hello");
+
+let s;
+console.log(typeof s);
+s = null;
+console.log(typeof s);
+s = {};
+console.log(typeof s);
+
+const a =10;
+const b =20;
+
+console.log(a+b);
