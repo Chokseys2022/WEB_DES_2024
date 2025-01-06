526ec1597f1e36923fea5003a5b9e841064bac26
SC <chokseysd@gmail.com>
2025-01-06 12:18:36 -0800

    part 2 done

--- 308/ALAB 308.3.1 Practical Loops/scripts/index.js
+++ 308/ALAB 308.3.1 Practical Loops/scripts/index.js
@@ -45,3 +45,32 @@ while (true) {
 
   n++;
 }
+
+/*Part 3: Feeling Loopy
+As a nal task, solve the following practical problem regarding string processing.
+Context: A CSV le, or “Comma-Separated Values” le is traditionally used to store tabular data. You
+may be familiar with CSVs through past use of programs such as Microsoft Excel or Google Sheets.
+While each of these programs save their data in dierent formats to preserve style (e.g., font color or
+cell backgrounds), at their core, they are storing CSV data.
+CSV data looks like this:
+ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz
+Master,58\n98,Bill,Doctor’s Assistant,26
+
+Loop through the characters of a given CSV string.
+Store each “cell” of data in a variable.
+When you encounter a comma, move to the next cell.
+When you encounter the “\r\n” sequence, move to the next “row.”
+Log each row of data.
+You do not need to format the data, the following works well.
+console.log(cell1, cell2, cell3, cell4);
+There will only be 4 cells per row.
+There will be no escaped characters other than “\n”*/
+
+const csvStr = `ID,Name,Occupation,Age
+42,Bruce,Knight,41
+57,Bob,Fry Cook,19
+63,Blaine,Quiz Master,58
+98,Bill,Doctor’s Assistant,26`;
+
+
+
